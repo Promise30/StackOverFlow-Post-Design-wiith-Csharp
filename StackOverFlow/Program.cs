5b62f1d5383556151d251ee14bd731d163de4db3
Promise30 <promiseadeagbo@gmail.com>
2023-03-05 15:20:39 +0100

    Updated by creating an instance of Post class

---
+++ StackOverFlow/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverFlow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Post post = new Post("The Post", "A New York Times best selling book of the 21st century.");
+
+            var _initialCount = post._voteCount;
+            Console.WriteLine("Inital number of votes: {0}", _initialCount);
+
+            post.UpVote();
+
+            var _currentCount = post._voteCount;
+            Console.WriteLine("Current number of votes: {0}", _currentCount);
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
